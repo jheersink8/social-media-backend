83fd8fc3c20974b56fc534d3d620d09fc52f6637
jheersink8 <150620321+jheersink8@users.noreply.github.com>
2024-04-27 15:46:07 -0600

    Added more basics

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,8 @@
+// Import Mongoose 
 const { connect, connection } = require('mongoose');
-
+// Connect to local instance of MongoDB
 const connectionString = 'mongodb://127.0.0.1:27017/socialDB';
 
 connect(connectionString);
-
+// Export connection
 module.exports = connection;
